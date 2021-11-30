50fe87c3572c3efbc9a65d1bdb333fa5eaacf9ca
elharsam <mohamed-ali.el-harsal@etu.univ-grenoble-alpes.fr>
2021-12-01 00:38:52 +0100

    adding toggle buttons for tts and vibration

--- app/src/main/java/com/example/pikalti/BicycleModeActivity.java
+++ app/src/main/java/com/example/pikalti/BicycleModeActivity.java
@@ -1,7 +1,5 @@
 package com.example.pikalti;
 
-import static android.Manifest.permission.INTERNET;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +9,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.util.Log;
+import android.speech.tts.TextToSpeech;
 import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ToggleButton;
 
@@ -23,6 +22,7 @@ import com.example.pikalti.lib.grelib.GestureRecognitionResponseListener;
 import com.example.pikalti.lib.grelib.RemoteGestureRecognitionClient;
 
 import java.util.List;
+import java.util.Locale;
 
 
 public class BicycleModeActivity extends AppCompatActivity {
@@ -30,15 +30,18 @@ public class BicycleModeActivity extends AppCompatActivity {
 
     private static final String TAG = "BicycleModeActivity";
     private static final int REQUEST_INTERNET = 0;
-    private ArrayAdapter<String> gesturesListAdapter = null;
-    private ListView recognizedGesturesList;
     private ToggleButton toggleButton = null;
     private Vibrator vibrator;
     private int menu = 0;
     private int menuItem = 0;
-
+    private boolean firstTimeInMenu = true;
+    private TextToSpeech t1;
     private GestureRecognitionClient gestureRecognitionClient;
 
+    private Button ttsButton;
+    private Button vibrationButton;
+    private int ttsFlag = 1;
+    private int vibrationFlag = 1;
 
 
     @Override
@@ -48,11 +51,38 @@ public class BicycleModeActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         gestureRecognitionClient = new RemoteGestureRecognitionClient(this);
         gestureRecognitionClient.setReferenceMode(ClientReferenceMode.DEVICE_REFERENCE);//change this to USER_FACING if needed.
-        gesturesListAdapter = new ArrayAdapter<>(this,R.layout.gesture_item);
-        recognizedGesturesList = findViewById(R.id.recognizedGesturesList);
-        recognizedGesturesList.setAdapter(gesturesListAdapter);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         gestureRecognitionClient.connect("wss://sdk.motiongestures.com/recognition?api_key=rJwpH669ntncdi12KOoJrgiDqWgo5fJVUihM3IZdWKdGOvbKtT");
+
+
+        ttsButton = (Button) findViewById(R.id.toggleTTS);
+        ttsButton.setOnClickListener(v -> {
+            if (ttsFlag == 0) {
+                ttsFlag = 1; // 1 => Button ON
+                ttsButton.setBackgroundResource(R.drawable.unmute);
+            } else {
+                ttsFlag = 0; // 0 => Button OFF
+                ttsButton.setBackgroundResource(R.drawable.mute);
+            }
+        });
+
+        vibrationButton = (Button) findViewById(R.id.toggleVibration);
+        vibrationButton.setOnClickListener(v -> {
+            if (vibrationFlag == 0) {
+                vibrationFlag = 1; // 1 => Button ON
+                vibrationButton.setBackgroundResource(R.drawable.vibration);
+            } else {
+                vibrationFlag = 0; // 0 => Button OFF
+                vibrationButton.setBackgroundResource(R.drawable.unvibration);
+            }
+        });
+
+
+        t1 = new TextToSpeech(getApplicationContext(), status -> {
+            if (status != TextToSpeech.ERROR) {
+                t1.setLanguage(Locale.UK);
+            }
+        });
 
 
         gestureRecognitionClient.setGestureRecognitionResponseListener(new GestureRecognitionResponseListener() {
@@ -60,9 +90,9 @@ public class BicycleModeActivity extends AppCompatActivity {
             public void gesturesRecognized(final List<String> names, final List<Integer> labels, float confidence) {
                 vibrate();
                 runOnUiThread(() -> {
-                    int size = Math.min(names.size(),labels.size());
-                    for(int i =0;i<size;i++) {
-                        gesturesListAdapter.add("Recognized gesture " + names.get(i) + " with label " + labels.get(i));
+                    int size = Math.min(names.size(), labels.size());
+                    for (int i = 0; i < size; i++) {
+//                        gesturesListAdapter.add("Recognized gesture " + names.get(i) + " with label " + labels.get(i));
                         executeAction(labels.get(i));
 
                     }
@@ -73,9 +103,9 @@ public class BicycleModeActivity extends AppCompatActivity {
             public void gesturesRejected(final List<String> names, final List<Integer> labels, float confidence) {
                 vibrate();
                 runOnUiThread(() -> {
-                    int size = Math.min(names.size(),labels.size());
-                    for(int i =0;i<size;i++) {
-                        gesturesListAdapter.add("Rejected gesture " + names.get(i) + " with label " + labels.get(i));
+                    int size = Math.min(names.size(), labels.size());
+                    for (int i = 0; i < size; i++) {
+//                        gesturesListAdapter.add("Rejected gesture " + names.get(i) + " with label " + labels.get(i));
                     }
                 });
             }
@@ -83,7 +113,7 @@ public class BicycleModeActivity extends AppCompatActivity {
             @Override
             public void gestureTooLong() {
                 vibrate();
-                gesturesListAdapter.add("Gesture Too Long");
+//                gesturesListAdapter.add("Gesture Too Long");
             }
 
         });
@@ -91,16 +121,44 @@ public class BicycleModeActivity extends AppCompatActivity {
 
     private void executeAction(Integer gestureLabel) {
 
-        switch (gestureLabel){
-            case 4:
-                menu = (menu == 0) ? 2 : menu-1;
-
-                System.out.println(Data.menueTitles[menu]);
+        switch (gestureLabel) {
+            case 2: //limmen chnage menue item
+                menuItem = (Data.countMenuTitles[menu] - 1 == menuItem) ? 0 : menuItem + 1;
+                menuItem = firstTimeInMenu ? 0 : menuItem;
+                firstTimeInMenu = false;
+                System.out.println(Data.countMenuTitles[menu]);
+                System.out.println(menuItem);
+                System.out.println(Data.menues[menu][menuItem]);
+                textToSpeach(Data.menues[menu][menuItem]);
                 break;
-            case 5:
-                menu = (menu == 2) ? 0 : menu+1;
 
-                System.out.println(Data.menueTitles[menu]);
+            case 3: //lisser chnage menue item
+                menuItem = (menuItem == 0) ? Data.countMenuTitles[menu] - 1 : menuItem - 1;
+                menuItem = firstTimeInMenu ? 0 : menuItem;
+                firstTimeInMenu = false;
+                System.out.println(menuItem);
+                System.out.println(Data.menues[menu][menuItem]);
+                textToSpeach(Data.menues[menu][menuItem]);
+                break;
+
+            case 4: //lisser chnage menu
+                menu = (menu == 0) ? 2 : menu - 1;
+                menuItem = 0;
+                firstTimeInMenu = true;
+                System.out.println(Data.menuTitles[menu]);
+                textToSpeach(Data.menuTitles[menu]);
+                break;
+            case 5: //limen chnage menu
+                menu = (menu == 2) ? 0 : menu + 1;
+                menuItem = 0;
+                firstTimeInMenu = true;
+                System.out.println(Data.menuTitles[menu]);
+                textToSpeach(Data.menuTitles[menu]);
+                break;
+
+            case 1:
+                textToSpeach(Data.menuAction[menu] + Data.menues[menu][menuItem]);
+                firstTimeInMenu = false;
                 break;
             default:
                 System.out.println("khrejt");
@@ -111,7 +169,10 @@ public class BicycleModeActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         gestureRecognitionClient.pause();
-        toggleButton.setChecked(false);
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
 
         super.onPause();
     }
@@ -124,7 +185,8 @@ public class BicycleModeActivity extends AppCompatActivity {
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_INTERNET) {
@@ -132,19 +194,24 @@ public class BicycleModeActivity extends AppCompatActivity {
         }
     }
 
-    public void vibrate(){
+    public void vibrate() {
+        if (vibrationFlag == 1) {
+            Runnable runnable = () -> {
+                if (Build.VERSION.SDK_INT >= 26) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    vibrator.vibrate(100);
+                }
+            };
+            Thread mythread = new Thread(runnable);
+            mythread.start();
+        }
 
-        Runnable runnable = () -> {
-            if (Build.VERSION.SDK_INT >= 26) {
-                vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE));
-            }else {
-                vibrator.vibrate(100);
-            }
-        };
-        Thread mythread = new Thread(runnable);
-        mythread.start();
     }
 
+    public void textToSpeach(String tts) {
+        if (ttsFlag == 1) t1.speak(tts, TextToSpeech.QUEUE_FLUSH, null);
+    }
 
 
 }
--- app/src/main/java/com/example/pikalti/Data.java
+++ app/src/main/java/com/example/pikalti/Data.java
@@ -5,15 +5,14 @@ import java.util.List;
 
 public class Data {
 //    public static String[] musics= new ArrayList<>();
-    public static String[] menueTitles={"musics", "contacts", "navigations"};
+    public static String[] menuTitles ={"musics", "contacts", "navigations"};
+    public static String[] menuAction ={"Playing ", "Calling ", "Navigation started to "};
+    public static int[] countMenuTitles={5, 8, 6};
 
     public static String[][] menues={
         {"Rain Over me by pitbul", "Die for you album valorant", "music 3", "music 4", "music 5"},
-        {"Sova", "Raze", "Reyna", "Chamber", "Killjoy"},
-        {"home", "work", "Haven", "Icebox", "Bind"}
-};
-//    public static String[] musics={"scoobidoobidooo", "helikhoptar helikhooptaar", "OMAGAAAAH", "music 4", "music 5"};
-//    public static String[] contacts={"Sova", "Raze helikhooptaar", "Reyna", "Chamber", "Killjoy"};
-//    public static String[] navigations={"Split", "Ascent", "Haven", "Icebox", "Bind"};
+        {"Sova", "Raze", "Reyna", "Chamber", "Killjoy", "Yoru", "Sage", "Omen"},
+        {"home", "work", "Ascent", "Haven", "Icebox", "Bind"}
+    };
 
 }
