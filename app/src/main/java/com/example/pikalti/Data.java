80b7fb35970c36e18729380f0b78eca884f46ef0
elharsam <mohamed-ali.el-harsal@etu.univ-grenoble-alpes.fr>
2021-12-01 09:36:19 +0100

    changing bike mode icon and data

--- app/src/main/java/com/example/pikalti/Data.java
+++ app/src/main/java/com/example/pikalti/Data.java
@@ -7,12 +7,12 @@ public class Data {
 //    public static String[] musics= new ArrayList<>();
     public static String[] menuTitles ={"musics", "contacts", "navigations"};
     public static String[] menuAction ={"Playing ", "Calling ", "Navigation started to "};
-    public static int[] countMenuTitles={5, 8, 6};
+    public static int[] countMenuTitles={5, 6, 5};
 
     public static String[][] menues={
-        {"Rain Over me by pitbul", "Die for you album valorant", "music 3", "music 4", "music 5"},
-        {"Sova", "Raze", "Reyna", "Chamber", "Killjoy", "Yoru", "Sage", "Omen"},
-        {"home", "work", "Ascent", "Haven", "Icebox", "Bind"}
+        {"Music 1", "music 2", "music 3", "music 4", "music 5"},
+        {"James", "Robert", "John", "Mary", "Patricia", "Jennifer"},
+        {"home", "work", "Restaurant", "Walmart Super Market", "Target super market"}
     };
 
 }
